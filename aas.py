0fbb0031bcf0413ba60a45dfcd367e23fcf2e81b
Robert Simai <robert.simai@suse.com>
2015-12-11 12:48:54 +0100

    done

--- aas.py
+++ aas.py
@@ -3,27 +3,31 @@
 #
 # rsimai@suse.com
 
-import string
+from string import upper, lower, ascii_lowercase
+from random import randint
 
 with open('/usr/share/dict/american') as fd:
     a =fd.read().splitlines()
 
 letter = ['']
+results = ['']
 count = 0
 word = ''
 
-print "Words", len(a)
-
 for i in range(0,26):
-    x = string.ascii_lowercase[i]
-    print "now for", x
+    x = ascii_lowercase[i]
+    mark1 = count
     while True:
         word = a[count]
-        if str.lower(word[:1]) == x:
+        if lower(word[:1]) == x:
             letter.append(word)
             count += 1
         else:
-            print "Letter", x, "close at", count
+            mark2 = count - 1
+            rand = randint(mark1, mark2)
+            first = upper(a[rand][:1])
+            out = first + "aaS : " + first + a[rand][1:] + " as a Service"
+            print out
             break
             
     
